909e67640aaf1017eae8365435195db9b13efc7f
74309 <743095@student.dusd.net>
2025-01-14 11:45:31 -0800

    AYAy
    vfvgsedf

--- WayPoints&GraphsGonzalezP-3/Assets/WPManager.cs
+++ WayPoints&GraphsGonzalezP-3/Assets/WPManager.cs
@@ -14,10 +14,25 @@ public class WPManager : MonoBehaviour
 {
     public GameObject[] wwaypoints;
     public Link[] links;
+    public Graphs graph = new Graphs();
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (wwaypoints.Length > 0)
+        {
+            foreach (GameObject wp in wwaypoints)
+            {
+                graph.AddNode(wp);
+            }
+            foreach (Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if (l.dir == Link.directionn.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
